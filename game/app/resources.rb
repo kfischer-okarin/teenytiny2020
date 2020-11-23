fe53397b83ddc64f65707ae2a662a4ea48b80d02
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-23 14:33:17 +0900

    Add mars  texture

--- game/app/resources.rb
+++ game/app/resources.rb
@@ -2,5 +2,6 @@
 module Resources
   group :sprites, 'png' do
     add :particle, w: 512, h: 512, author: 'Kenney', source: 'Assets 3/2D/Particles/circle_05'
+    add :mars, w: 512, h: 512, author: 'mafon2', source: 'https://opengameart.org/content/seamless-space-rocks-textures-pack-512px'
   end
 end
